b7f30f3dc9c43c57822a58e6ebed59576cf0cc88
ccj-k-nagata <k_nagata@carekarte.com>
2026-04-08 13:44:41 +0900

    開発体験授業用のコミットです。

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // 【ワーク】ここを「Javaの勉強を頑張ります！」に書き換えてみましょう
-        System.out.println("Hello World");
+        System.out.println("Javaの勉強を頑張ります！");
     }
 }
